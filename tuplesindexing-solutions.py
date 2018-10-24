fedf80dd6bcf2031d492cbc9b72431fbd80c3cf8
quanta413 <nicholas.alexei.sherer@gmail.com>
2018-10-24 11:24:50 -0500

    added a space to comments to stop Spyder from complaining about PEP 8 violation

--- tuplesindexing-solutions.py
+++ tuplesindexing-solutions.py
@@ -1,4 +1,4 @@
-#access the number 7 in the following set of tuples.
+# access the number 7 in the following set of tuples.
 sevensTuple0 = (0, 3, 4, 7, 3)
 sevensTuple1 = (0, 9, 5, 2, (7, 1))
 sevensTuple2 = (1, (4,), ('cat', (5, (8, 7) ) ), 13)
@@ -7,5 +7,5 @@ zerothSeven = sevensTuple0[3]
 onethSeven = sevensTuple1[4][0]
 twothSeven = sevensTuple2[2][1][1][1]
 
-#print out the number you access so can see your answer.
+# print out the numbers you access so can see your answer.
 print(zerothSeven, onethSeven, twothSeven)
